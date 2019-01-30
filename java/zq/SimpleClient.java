d0630da02c3851cd7e3cc991fe5b682bbf469f99
wendll <27952278@qq.com>
2019-01-30 14:39:53 +0800

    Add SimpleClient and SimpleServer to handle static resource access

---
+++ java/zq/SimpleClient.java
@@ -0,0 +1,45 @@
+package zq;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Run Bootstrap class firstly
+ * And then run SimpleClient
+ */
+
+public class SimpleClient {
+    public static void main(String[] args) throws Exception{
+        Socket socket = new Socket("127.0.0.1", 8080);
+        OutputStream os = socket.getOutputStream();
+        boolean autoFlush = true;
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), autoFlush);
+        BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        out.println("GET /index.jsp HTTP/1.1");
+        out.println("Host: localhost:8080");
+        out.println("Connection: Close");
+        out.println();
+
+        boolean loop = true;
+        StringBuffer sb = new StringBuffer(8096);
+        while (loop) {
+            if ( in.ready() ) {
+                int i = 0;
+                while (i != -1) {
+                    i = in.read();
+                    sb.append((char) i);
+                }
+                loop = false;
+            }
+            Thread.currentThread().sleep(50);
+        }
+
+        System.out.println(sb.toString());
+        socket.close();
+    }
+}
---
+++ java/zq/domain/Response.java
@@ -0,0 +1,59 @@
+package zq.domain;
+
+import zq.SimpleServer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class Response {
+
+    private static final int BUFFER_SIZE = 1024;
+
+    private OutputStream out;
+
+    private Request request;
+
+    public Response(OutputStream out) {
+        this.out = out;
+    }
+
+    public void setRequest(Request request) {
+        this.request = request;
+    }
+
+
+    public void sendStaticResource() throws IOException{
+        byte [] bytes = new byte[BUFFER_SIZE];
+
+        FileInputStream fis = null;
+
+        try {
+            File file = new File(SimpleServer.WEB_ROOT, request.getUri());
+            if (file.exists()) {
+                String responseHeader = "HTTP/1.1 200\r\n" +
+                        "Content-Type: text/html\r\n" +
+                        "\r\n";
+                out.write(responseHeader.getBytes());
+                fis = new FileInputStream(file);
+                int ch = fis.read(bytes, 0 , BUFFER_SIZE);
+                while (ch != -1) {
+                    out.write(bytes, 0, ch);
+                    ch = fis.read(bytes, 0 , BUFFER_SIZE);
+                }
+            } else {
+                String errorResponse = "HTTP/1.1 404 File Not Found\r\n" +
+                        "Content-Type: text/html\r\n" +
+                        "Content-Length: 23\r\n" +
+                        "\r\n" +
+                        "<h1>File Not Found</h1>";
+                out.write(errorResponse.getBytes());
+            }
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        } finally {
+            fis.close();
+        }
+    }
+}
